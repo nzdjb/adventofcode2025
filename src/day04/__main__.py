5fbf82005ec4a5349b9200a69e43e5a6442e8b63
nzdjb <github@2a1b.com>
2025-12-01 08:32:31 +0000

    feat: Update templated mains to use util.

--- src/day04/__main__.py
+++ src/day04/__main__.py
@@ -1,4 +1,7 @@
+from ..util import load_input
+
 from .solution import solve_part1, solve_part2
 
-solve_part1("")
-solve_part2("")
+
+print(solve_part1(load_input()))
+print(solve_part2(load_input()))
