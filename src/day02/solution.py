cb020141e98879afbb9ca4f3db1cf1d99bea9de5
nzdjb <github@2a1b.com>
2025-12-02 08:21:03 +0000

    feat: day02p2

--- src/day02/solution.py
+++ src/day02/solution.py
@@ -15,4 +15,20 @@ def solve_part1(data: str) -> str:
 
 
 def solve_part2(data: str) -> str:
-    return ""
+    total = 0
+    candidate_ranges = data.split(",")
+    for candidate_range in candidate_ranges:
+        [start, end] = candidate_range.split("-")
+        candidate_generator = range(int(start), int(end) + 1)
+        for candidate in candidate_generator:
+            str_candidate = str(candidate)
+            str_candidate_length = len(str_candidate)
+            for group_size in range(1, (str_candidate_length // 2) + 1):
+                num_groups = str_candidate_length // group_size
+                if num_groups * group_size != str_candidate_length:
+                    continue
+                if str_candidate[:group_size] * num_groups == str_candidate:
+                    total += candidate
+                    break
+
+    return str(total)
