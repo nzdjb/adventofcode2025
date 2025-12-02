bf75a8365c10bfab9c2cee81961e09e7b2e57e67
nzdjb <github@2a1b.com>
2025-12-02 07:52:35 +0000

    feat: day02p1

--- src/day02/solution.py
+++ src/day02/solution.py
@@ -1,5 +1,17 @@
 def solve_part1(data: str) -> str:
-    return ""
+    total = 0
+    candidate_ranges = data.split(",")
+    for candidate_range in candidate_ranges:
+        [start, end] = candidate_range.split("-")
+        candidate_generator = range(int(start), int(end) + 1)
+        for candidate in candidate_generator:
+            str_candidate = str(candidate)
+            if len(str_candidate) % 2 != 0:
+                continue
+            half = len(str_candidate) // 2
+            if str_candidate[half:] == str_candidate[:half]:
+                total += candidate
+    return str(total)
 
 
 def solve_part2(data: str) -> str:
--- src/day02/tests/test_solution.py
+++ src/day02/tests/test_solution.py
@@ -2,7 +2,12 @@ from pytest import mark
 
 from ..solution import solve_part1, solve_part2
 
-part1_samples = []
+part1_samples = [
+    (
+        "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124",
+        "1227775554",
+    )
+]
 
 
 @mark.parametrize("data,expected", part1_samples)
