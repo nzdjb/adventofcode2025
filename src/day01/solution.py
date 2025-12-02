c880b5b8b1be8cde10ba26fc486efa1c53c64194
nzdjb <github@2a1b.com>
2025-12-02 07:07:30 +0000

    feat: Partial day01p2 solution.

--- src/day01/solution.py
+++ src/day01/solution.py
@@ -1,6 +1,32 @@
+from math import floor
+from operator import add, sub
+import re
+
+
 def solve_part1(data: str) -> str:
-    return ""
+    dial = 50
+    zeroes = 0
+    data = data.splitlines()
+    for entry in data:
+        m = re.match("(?P<direction>[RL])(?P<quantity>[0-9]+)", entry)
+
+        op = sub if m["direction"] == "L" else add
+        dial = op(dial, int(m["quantity"])) % 100
+        if dial == 0:
+            zeroes = zeroes + 1
+    return str(zeroes)
 
 
 def solve_part2(data: str) -> str:
-    return ""
+    dial = 50
+    zeroes = 0
+    data = data.splitlines()
+    for entry in data:
+        m = re.match("(?P<direction>[RL])(?P<quantity>[0-9]+)", entry)
+
+        op = sub if m["direction"] == "L" else add
+        quan = int(m["quantity"])
+        zeroes += abs(floor(op(dial, quan) / 100))
+        dial = op(dial, quan) % 100
+
+    return str(zeroes)
--- src/day01/tests/test_solution.py
+++ src/day01/tests/test_solution.py
@@ -1,29 +1,52 @@
-from pytest import fixture
+from pytest import fixture, mark
 
 from ..solution import solve_part1, solve_part2
 
 
 @fixture
 def part1_sample():
-    data = """
-"""
-    expected = ""
+    data = """L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82"""
+    expected = "3"
     return [data, expected]
 
 
-def test_solve_part1(part1_sample):
+def test_solve_part1(part1_sample: list[str]):
     (data, expected) = part1_sample
     assert solve_part1(data) == expected
 
 
-@fixture
-def part2_sample():
-    data = """
-"""
-    expected = ""
-    return [data, expected]
+part2_samples = [
+    (
+        """L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82""",
+        "6",
+    ),
+    ("R1000", "10"),
+    (
+        """R40
+L20""",
+        "0",
+    ),
+]
 
 
-def test_solve_part2(part2_sample):
-    (data, expected) = part2_sample
+@mark.parametrize("data,expected", part2_samples)
+def test_solve_part2(data: str, expected: str):
     assert solve_part2(data) == expected
