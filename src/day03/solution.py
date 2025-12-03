040d91260858c1a1a51e794fadde8dfc51d328a7
nzdjb <github@2a1b.com>
2025-12-03 07:14:21 +0000

    feat: day03p1

--- src/day03/solution.py
+++ src/day03/solution.py
@@ -1,5 +1,11 @@
 def solve_part1(data: str) -> str:
-    return ""
+    total = 0
+    for line in data.splitlines():
+        battery_list = list(line)
+        max_volts = sorted(battery_list[:-1])[-1]
+        max_volts2 = sorted(battery_list[battery_list.index(max_volts)+1:])[-1]
+        total += int(f"{max_volts}{max_volts2}")
+    return str(total)
 
 
 def solve_part2(data: str) -> str:
--- src/day03/tests/test_solution.py
+++ src/day03/tests/test_solution.py
@@ -2,7 +2,18 @@ from pytest import mark
 
 from ..solution import solve_part1, solve_part2
 
-part1_samples = []
+part1_samples = [(
+    """987654321111111
+811111111111119
+234234234234278
+818181911112111""",
+    "357"
+),
+    ("987654321111111", "98"),
+    ("811111111111119", "89"),
+    ("234234234234278", "78"),
+    ("818181911112111", "92"),
+]
 
 
 @mark.parametrize("data,expected", part1_samples)
