9000431eb8ef44c552a50774d8779ff4fdaff377
nzdjb <github@2a1b.com>
2025-12-03 07:36:39 +0000

    feat: day03p2

--- src/day03/solution.py
+++ src/day03/solution.py
@@ -9,4 +9,15 @@ def solve_part1(data: str) -> str:
 
 
 def solve_part2(data: str) -> str:
-    return ""
+    total = 0
+    for line in data.splitlines():
+        battery_list = list(line)
+        for _ in range(len(battery_list)-12):
+            for i in range(len(battery_list)-1):
+                if battery_list[i+1] > battery_list[i]:
+                    del battery_list[i]
+                    break
+            else:
+                del battery_list[-1]
+        total += int("".join(battery_list))
+    return str(total)
