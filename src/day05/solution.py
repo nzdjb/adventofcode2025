d4cc4af58ccdaeef3a5d6cbaaea108b3e4220598
nzdjb <github@2a1b.com>
2025-12-05 09:50:44 +0000

    feat: day05p1

--- src/day05/solution.py
+++ src/day05/solution.py
@@ -1,5 +1,20 @@
 def solve_part1(data: str) -> str:
-    return ""
+    lines = data.splitlines()
+    split = lines.index("")
+    fresh = lines[:split]
+    ingredients = map(lambda x: int(x), lines[split+1:])
+    fresh_lists = []
+    for fresh_line in fresh:
+        fresh_lists.append([int(x) for x in fresh_line.split("-")])
+
+    fresh_ingredients = 0
+    for ingredient in ingredients:
+        for fresh_list in fresh_lists:
+            if ingredient >= fresh_list[0] and ingredient <= fresh_list[1]:
+                fresh_ingredients += 1
+                break
+
+    return str(fresh_ingredients)
 
 
 def solve_part2(data: str) -> str:
