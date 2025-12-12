20aaa76e1e2bd2829a190c47af014c749a010e19
nzdjb <github@2a1b.com>
2025-12-12 07:09:24 +0000

    feat: day07

--- src/day07/solution.py
+++ src/day07/solution.py
@@ -1,6 +1,33 @@
+from functools import cache
+
+
 def solve_part1(data: str) -> str:
-    return ""
+    split_count = 0
+    grid = [list(line) for line in data.splitlines()]
+    for y, line in enumerate(grid):
+        for x, char in enumerate(line):
+            if char == '.':
+                if grid[y-1][x] == 'S' or grid[y-1][x] == '|':
+                    grid[y][x] = '|'
+            elif char == '^':
+                if grid[y-1][x] == '|':
+                    split_count += 1
+                    grid[y][x-1] = '|'
+                    grid[y][x+1] = '|'
+
+    return str(split_count)
 
 
 def solve_part2(data: str) -> str:
-    return ""
+    grid = [list(line) for line in data.splitlines()]
+    limit = len(grid)
+
+    @cache
+    def beam(y: int, x: int) -> int:
+        if y >= limit:
+            return 1
+        if grid[y][x] == "^":
+            return beam(y+2, x-1) + beam(y+2, x+1)
+        return beam(y+1, x)
+
+    return str(beam(1, grid[0].index("S")))
