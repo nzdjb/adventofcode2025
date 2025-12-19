98f9e01fc413e9ef567601b2ab4811152902e544
nzdjb <github@2a1b.com>
2025-12-19 07:14:58 +0000

    feat: day08p1

--- src/day08/solution.py
+++ src/day08/solution.py
@@ -1,5 +1,35 @@
-def solve_part1(data: str) -> str:
-    return ""
+from functools import reduce
+from itertools import combinations
+from math import dist
+from operator import mul
+
+
+def solve_part1(data: str, connections: int = 1000) -> str:
+    coords = [tuple([int(pt) for pt in line.strip().split(",")])
+              for line in data.splitlines()]
+    combs = [(dist(*comb), comb) for comb in combinations(coords, 2)]
+    combs = map(lambda comb: comb[1], sorted(
+        combs, key=lambda comb: comb[0])[:connections])
+    circuits = []
+    for comb in combs:
+        for circ in circuits:
+            if comb[0] in circ or comb[1] in circ:
+                circ.add(comb[0])
+                circ.add(comb[1])
+                break
+        else:
+            circuits.append(set(comb))
+
+    completed_circuits = []
+    while len(circuits) > 0:
+        first, circuits = [circuits[0], circuits[1:]]
+        for circuit in circuits:
+            if not first.isdisjoint(circuit):
+                circuit.update(first)
+                break
+        else:
+            completed_circuits.append(first)
+    return str(reduce(mul, sorted(map(len, completed_circuits))[-3:]))
 
 
 def solve_part2(data: str) -> str:
--- src/day08/tests/test_solution.py
+++ src/day08/tests/test_solution.py
@@ -2,12 +2,31 @@ from pytest import mark
 
 from ..solution import solve_part1, solve_part2
 
-part1_samples = []
+part1_samples = [("""162,817,812
+57,618,57
+906,360,560
+592,479,940
+352,342,300
+466,668,158
+542,29,236
+431,825,988
+739,650,466
+52,470,668
+216,146,977
+819,987,18
+117,168,530
+805,96,715
+346,949,466
+970,615,88
+941,993,340
+862,61,35
+984,92,344
+425,690,689""", 10, "40")]
 
 
-@mark.parametrize("data,expected", part1_samples)
-def test_solve_part1(data: str, expected: str):
-    assert solve_part1(data) == expected
+@mark.parametrize("data,connections,expected", part1_samples)
+def test_solve_part1(data: str, connections: int, expected: str):
+    assert solve_part1(data, connections) == expected
 
 
 part2_samples = []
