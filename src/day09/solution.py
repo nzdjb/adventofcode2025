c153ae00c50c3c1aad10b9cdc0b385a9f69fc064
nzdjb <github@2a1b.com>
2025-12-20 06:58:16 +0000

    feat: day09p1

--- src/day09/solution.py
+++ src/day09/solution.py
@@ -1,5 +1,16 @@
+from itertools import combinations
+from math import dist
+
+
 def solve_part1(data: str) -> str:
-    return ""
+    coords = [
+        tuple([int(pt) for pt in line.strip().split(",")]) for line in data.splitlines()
+    ]
+    sizes = [
+        (abs(comb[0][0] - comb[1][0]) + 1) * (abs(comb[0][1] - comb[1][1]) + 1)
+        for comb in combinations(coords, 2)
+    ]
+    return str(max(sizes))
 
 
 def solve_part2(data: str) -> str:
--- src/day09/tests/test_solution.py
+++ src/day09/tests/test_solution.py
@@ -2,7 +2,20 @@ from pytest import mark
 
 from ..solution import solve_part1, solve_part2
 
-part1_samples = []
+part1_samples = [
+    (
+        """7,1
+11,1
+11,7
+9,7
+9,5
+2,5
+2,3
+7,3""",
+        "50",
+    ),
+    ("1,1\n3,3\n1,5", "9"),
+]
 
 
 @mark.parametrize("data,expected", part1_samples)
